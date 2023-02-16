75a56eafb02cbb47484245095dab36c5db12eb82
stacynduta <stacynduta19@gmail.com>
2023-02-16 09:25:16 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that outputs puts func
+ * Return:0(pass)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
