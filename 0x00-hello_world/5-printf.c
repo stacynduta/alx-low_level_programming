5e9545d3b9e03720a46e70be7b8c88764e36e6a5
stacynduta <stacynduta19@gmail.com>
2023-02-16 12:17:52 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main -enrty pnts
+ * main - Entry point
  *
- * Description: using sizeof to print the size of various types
+ * Description: A C program that prints with printf function.
  *
- * Return: 0 (pass)
+ * Return:0 (pass)
  */
 int main(void)
 {
-/* sizeof evaluates the size of a variable */
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
