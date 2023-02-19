75830510589c67f19e0f4d0ca90f72070cba5a25
stacynduta <stacynduta19@gmail.com>
2023-02-19 05:09:59 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 {
-	int n;
+	int c = 0;
 
-	for (n = 48; n < 58; n++)
+	while (c < 10;)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(48 + c);
+		if (c != 9)
 		{
 			putchar(',');
 			putchar('');
 		}
+		c++
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
