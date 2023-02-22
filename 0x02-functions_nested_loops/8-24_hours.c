978583182a9296ab1a0c618933f5d0197213f416
stacynduta <stacynduta19@gmail.com>
2023-02-22 06:49:38 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,26 +7,24 @@
 
 void jack_bauer(void)
 {
-        int a,b,c,d;
-        for (a = 0; a <= 2; a++)
-        {
-                for (b = 0; b <= 9; b++)
-                {
-                        if (( a <=1 && b <= 9) || ( a <= 2 && b <= 3))
-                        {
-                                for (c = 0; c<= 5; c++)
-                                {
-                                        for (d = 0; d <= 9; d++)
-                                        {
-                                                _putchar(a + '0');
-                                                _putchar(b + '0');
-                                                _putchar(58);
-                                                _putchar( c + '0');
-                                                _putchar(d + '0');
-                                                _putchar('\n');
-                                        }
-                                }
-                        }
-                }
-        }
+	int hour, minute;
+
+	for (hour =  0; hour < 24; ++hour)
+	{
+
+		for (minute = 0; minute < 60; ++minute)
+		{
+
+			_putchar ((hour / 10) + 48);
+			_putchar ((hour % 10) + 48);
+
+			_putchar (':');
+
+			_putchar ((minute / 10) + 48);
+			_putchar ((minute % 10) + 48);
+			_putchar ('\n');
+
+		}
+
+	}
 }
