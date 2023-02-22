d3d8619dcc9923628477633411cb40503e37497a
stacynduta <stacynduta19@gmail.com>
2023-02-22 09:38:53 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Natural
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
