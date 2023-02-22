0ae5d1ce471b285929345cde44c132b5343e30b7
stacynduta <stacynduta19@gmail.com>
2023-02-22 07:19:56 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,37 @@
 
 /**
  * times_table - print the a time table starting with 0
- * Return: emptu output
- *
+ * Return: empty output
  */
-void times-ta
+
+void times_table(void)
+{
+	int row, column;
+
+	for (row = 0; row < 10; ++row)
+	{
+		for (column = 0; column < 10; ++column)
+		{
+
+			int prd = row * column;
+
+			if (column > 0)
+			{
+				if (prd < 10)
+					_putchar(' ');
+				_putchar(' ');
+			}
+			if (prd >= 10)
+				_putchar((prd / 10) + 48);
+			_putchar((prd % 10) + 48);
+
+			if (column > 8)
+			{
+				_putchar('\n');
+			} else
+			{
+				_putchar(',');
+			}
+		}
+	}
+}
